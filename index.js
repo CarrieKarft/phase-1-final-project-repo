0c35277bef852d506f76b941193ec5222497f9c5
Carrie Kraft <carriekarft@gmail.com>
2023-01-26 15:39:44 -0800

    deleting psudeocode and comments to prepare for project review

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-// assigning elements to variable names in the global scope
-// grabbing form elements
+
 const form = document.querySelector('#bookForm')
 const [searchBar, submitButton] = form.children
-// console.log(submitButton)
-// console.log(searchBar)
-// grabbiing div containers for elements
+
 const results = document.getElementById('results')
 const readingList = document.querySelector('.readingListDiv');
 
 
 function getingUserInput() {
-    // 1 grab the text entered into search bar by user
     submitButton.addEventListener('click', e => {
-        // 2 when submit button 'clicked' (event) send that input text to our fetch request
         e.preventDefault()
         document.getElementById('results').innerHTML = ''
         let searchBarText = searchBar.value
@@ -23,10 +18,8 @@ function getingUserInput() {
 getingUserInput()
 
 function fetchinBookData(searchBarText) {
-    // 3 fetch specified book title form Open Library API
     return fetch(`https://openlibrary.org/search.json?title=${searchBarText}`)
     .then(resp => resp.json())
-    // 4 return data from api
     .then(bookData => {
         // console.log(bookData)
         bookData.numFound > 0 ? gettingTitleFromBookData(bookData) : noResultsFoundElement();
@@ -44,7 +37,6 @@ function noResultsFoundElement() {
 
 function gettingTitleFromBookData(bookData) {
     console.log(bookData)
-    // 5 parse through data and select title and author values 
     const docsArray = bookData.docs
     docsArray.map(element => {
         const bookObj = {
@@ -57,10 +49,7 @@ function gettingTitleFromBookData(bookData) {
 }
 
 function creatingBookResultElements(bookObj) {
-    // console.log(bookObj.subjects)
-    // 6 send these values to function to create elements and apppend these values to DOM for each book
     const resultDiv = document.createElement('div');
-    // 7 create event listener when creating elements that listens for the 'dragend' event of user dragging search result elemt over to reading list
     resultDiv.addEventListener('dragend', () => {
         creatingTheReadingListElements(bookObj);
     })
@@ -84,9 +73,7 @@ function creatingBookResultElements(bookObj) {
 }
 
 function creatingTheReadingListElements(bookObj) {
-    // 8 when event is fired take book that event was triggerd for and send data to function to create element in reading list and append to DOM
     const bookListDiv = document.createElement('div');
-    // 9 when creating reading list element add event listener 'mouseover' that enlarges the text of the target of the event
     bookListDiv.textContent = bookObj.bookTitle + "  ";
     const btn = document.createElement('button');
     btn.textContent = "X"
@@ -109,46 +96,9 @@ function addingEventListenersToReadingList(bookListDiv, btn) {
         eTarget.style.fontSize = '16px'
         btn.style.display = "none"
     })
-    // 10 when creating reading list element also add event listener that listens for the 'click' event of user clicking X button and deletes the targeted element from the reading list
     btn.addEventListener('click', () => {
         const readingElementDiv = btn.parentElement
         readingElementDiv.remove()
     })
 }
-
-
-/* Psudeo Code
-1 grab the text entered into search bar by user
-2 when submit button 'clicked' send that input text to our fetch request
-3 fetch specified book title form Open Library API
-4 return data from api
-5 parse through data and select title and author values 
-6 send these values to function to create elements and apppend these values to DOM for each book
-7 create event listener when creating elements that listens for the 'keydown' event of user pressing up arrow
-8 when event is fired take book that event was triggerd for and send data to function to create element in reading list and append to DOM
-9 when creating reading list element add event listener 'mouseover' that enlarges the text of the target of the event
-10 when creating reading list element also add event listener that listens for the 'keydown' event of user pressing down arrow and delets the targeted element from the reading list
-*/
-
-/*
-Click event
-To click the search button to look up a book in the API
-
-Dragend event
-To add a book to their read later list, and delete book from read later list
-
-Mouseover event (mouseenter and mouseleave)
-To enlarge the title text of each book once it is in their read later list (just in case the user forgot to wear their reading glasses)
-*/
-
-
-/*
-    The core features	will include:
-A search bar where a user can enter in the title of the book that they are looking for.
-Once they have searched for a book the results of that search will be displayed on the webpage.
-The user will then be able to add that book to a list of books to read later by hitting the up arrow on their keyboard.
-The users read later list will be located on a separate portion of the page 
-If the user scrolls over the title of the books in their read later list the title text will appear larger for better visibility.
-The user will also be able to delete books from their read later list by pressing the down arrow key.
-*/
 
